9ae3a086bbd98737082892aa2d41b83a5de8c726
dusgn <33680794+gilthub@users.noreply.github.com>
2022-04-09 11:36:52 +0900

    init commit

---
+++ src/main/java/com/example/toy_1st_demo/Toy1stDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_1st_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy1stDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy1stDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toy_1st_demo/Toy1stDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toy_1st_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy1stDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
